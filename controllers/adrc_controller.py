d41590ceaeda1c6e47fa201dfeef132849ea7717
Kamil Markowski <Kamkusik90@gmail.com>
2025-04-21 16:51:30 +0200

    Task 3 - beggining

--- controllers/adrc_controller.py
+++ controllers/adrc_controller.py
@@ -1,4 +1,6 @@
 import numpy as np
+
+from models.manipulator_model import ManiuplatorModel
 from .adrc_joint_controller import ADRCJointController
 from .controller import Controller
 
@@ -6,12 +8,17 @@ from .controller import Controller
 class ADRController(Controller):
     def __init__(self, Tp, params):
         self.joint_controllers = []
+        self.model = ManiuplatorModel(Tp)
         for param in params:
             self.joint_controllers.append(ADRCJointController(*param, Tp))
 
     def calculate_control(self, x, q_d, q_d_dot, q_d_ddot):
         u = []
+        M = self.model.M(x)
+        invM = np.linalg.inv(M)
+        # print(np.diagonal(M))
         for i, controller in enumerate(self.joint_controllers):
+            controller.set_b(np.diagonal(M)[i])
             u.append(controller.calculate_control([x[i], x[i+2]], q_d[i], q_d_dot[i], q_d_ddot[i]))
         u = np.array(u)[:, np.newaxis]
         return u
--- controllers/adrc_joint_controller.py
+++ controllers/adrc_joint_controller.py
@@ -9,16 +9,23 @@ class ADRCJointController(Controller):
         self.kp = kp
         self.kd = kd
 
-        A = None
-        B = None
-        L = None
-        W = None
+        A = np.array([[0, 1, 0],
+                      [0, 0, 1],
+                      [0, 0, 0]])
+        B = np.array([[0],
+                      [b],
+                      [0]])
+        L = np.array([[3 * p], [3 * p**2], [p**3]])
+        W = np.array([[1, 0, 0]])
         self.eso = ESO(A, B, W, L, q0, Tp)
 
     def set_b(self, b):
-        ### TODO update self.b and B in ESO
-        return NotImplementedError
+        self.b = b
+        self.eso.set_B(np.array([[0], [b], [0]]))
 
     def calculate_control(self, x, q_d, q_d_dot, q_d_ddot):
-        ### TODO implement ADRC
-        return NotImplementedError
+        _, q_h_dot, f = self.eso.get_state()
+        v = self.kp * (q_d - x[0]) +  self.kd * (q_d_dot - q_h_dot) + q_d_ddot 
+        u = (v - f) / self.b
+        self.eso.update(x[0], u)
+        return u
